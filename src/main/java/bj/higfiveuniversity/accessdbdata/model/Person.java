fc7c63d237dbd4d9c3a6c0122b041fa657376d22
Chirac Michoagan <michoaganchiracdona@gmail.com>
2024-08-28 09:33:40 +0100

    c

--- src/main/java/bj/higfiveuniversity/accessdbdata/model/Person.java
+++ src/main/java/bj/higfiveuniversity/accessdbdata/model/Person.java
@@ -1,10 +1,17 @@
- @Entity
+package bj.higfiveuniversity.accessdbdata.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
  public class Person {
-    @id
-    @GeneratedValue(strategy = GeneraType.Auto)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String firstName;
-    private String  lasttName;
+    private String  lastName;
 
 
     public String getFirstName(){
@@ -13,11 +20,11 @@
     public void setFirstName( String firstName){
        this.firstName = firstName;
     }
-    public String getLasttName(){
-        return  lasttName;
+    public String getLastName(){
+        return  lastName;
     }
-    public String setLasttName(String  lasttName){
-        this.lastName = lasttName;
+    public String setLastName(String  lastName){
+        this.lastName = lastName;
     }
 
 
